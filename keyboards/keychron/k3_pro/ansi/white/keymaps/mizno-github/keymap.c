e6c0d16db0c128392bea71d14897372973a29c25
水野悠哉 <mizno911.zeta@gmail.com>
2026-05-04 21:58:31 +0900

    fix: pgupなどの誤タイピングを防止する

--- keyboards/keychron/k3_pro/ansi/white/keymaps/mizno-github/keymap.c
+++ keyboards/keychron/k3_pro/ansi/white/keymaps/mizno-github/keymap.c
@@ -151,19 +151,19 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      [MAC_BASE] = LAYOUT_ansi_84(
           KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,     KC_F12,  KC_SNAP,  KC_DEL,  KC_F13,
-          KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,    KC_EQL,  KC_BSPC,           KC_PGUP,
-          KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,    KC_RBRC, KC_BSLS,           KC_PGDN,
-          KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,             KC_ENT,            KC_HOME,
-          KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,             KC_RSFT,  KC_UP,   KC_END,
+          KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,    KC_EQL,  KC_BSPC,           KC_NO,
+          KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,    KC_RBRC, KC_BSLS,           KC_NO,
+          KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,             KC_ENT,            KC_NO,
+          KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,             KC_RSFT,  KC_UP,   KC_NO,
           0,        0,        0,                                      KC_SPC,                                 0,        MO(MAC_FN), KC_RCTL, KC_LEFT,  KC_DOWN, KC_RGHT
      ),
 
      [MAC_FN] = LAYOUT_ansi_84(
           KC_ESC,   KC_BRID,  KC_BRIU,  KC_MCTL,  KC_LPAD,  BL_DOWN,  BL_UP,    KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,  KC_SNAP,  KC_DEL,   BL_STEP,
-          KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,
-          KC_TRNS,  S(KC_1),  0,        S(KC_3),  S(KC_4),  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,
-          KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  BL_DOWN,  BL_UP,    BL_STEP,  BL_TOGG,  KC_TRNS,            KC_TRNS,            KC_TRNS,
-          KC_TRNS,            KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  BAT_LVL,  NK_TOGG,  KC_TRNS,  BT_HST1,  BT_HST2,  BT_HST3,            KC_TRNS,  KC_TRNS,  KC_TRNS,
+          KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_PGUP,
+          KC_TRNS,  S(KC_1),  0,        S(KC_3),  S(KC_4),  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_PGDN,
+          KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  BL_DOWN,  BL_UP,    BL_STEP,  BL_TOGG,  KC_TRNS,            KC_TRNS,            KC_HOME,
+          KC_TRNS,            KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  BAT_LVL,  NK_TOGG,  KC_TRNS,  BT_HST1,  BT_HST2,  BT_HST3,            KC_TRNS,  KC_TRNS,  KC_END,
           KC_TRNS,  KC_TRNS,  KC_TRNS,                                KC_TRNS,                                KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS
      ),
 
